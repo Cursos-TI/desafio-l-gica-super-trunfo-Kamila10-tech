90f8e7b1c62c40de1aa15e4a8e1e8b95d7a2cc76
Kamila10-tech <kamilaarchilia97@hotmail.com>
2025-08-24 23:23:23 +0000

    comentarios finais

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -4,9 +4,11 @@
 
 int main() {
 
+    // linha de ínicio do programa
     printf("Bem-vindo a Lógica do Super Trunfo!\n");
 
-  char nome1[50] = "SP";
+    // adicionando os valores de cada item
+    char nome1[50] = "SP";
     char codigocarta1[20] = "SP1";
     char nomedacidade[50] = "Sao Paulo";
     unsigned int populacao1 = 12400000;
@@ -21,11 +23,15 @@ int main() {
     float PIB2 = 600.0;
     unsigned int numeropontosturisticos2 = 8;
 
+    // cadastrando informações que não se alteram
             printf(" CARTA 1: São Paulo possui 12.400.000 habitantes.\n");
             printf(" CARTA 2: Rio de Janeiro possui 6.211.423 habitantes.\n");
 
+    // se a pupulaçao 1 for maior que a população 2 
         if (populacao1 > populacao2) {
             printf("A carta 1 é a vencedora!\n");
+
+    // se a popupação 1 não for maior que a população 2
         } else {
             printf("A carta 2 é a vencedora!\n");
         }
